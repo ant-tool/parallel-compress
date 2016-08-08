5d4158c8f67bf0e6a22fd143a97774f7d8862c2e
xiaosheng.lj <xiaosheng.lj@alibaba-inc.com>
2016-08-08 14:03:18 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# paraller-compress
-compress js paraller
+# parallel-compress
+
+## Usage
+
+```
+const compress = require('parallel-compress');
+webpackConfig.plugins.push(new compress());
+```
+
+## Notice
+
+不需要 uglify 插件: `webpack.optimize.UglifyJsPlugin`
+
+```
+webpackConfig.plugins.some(function(plugin, i) {
+  if (plugin instanceof webpack.optimize.UglifyJsPlugin) {
+    webpackConfig.plugins.splice(i, 1);
+    return true;
+  }
+});
+```
+
+
---
+++ index.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const workerFarm = require('worker-farm');
+const maxConcurrentWorkers = require('os').cpus().length;
+const SourceMapConsumer = require("source-map").SourceMapConsumer;
+const RawSource = require("webpack-sources").RawSource;
+const SourceMapSource = require("webpack-sources").SourceMapSource;
+
+function Compress(options) {
+  this.options = options || {};
+}
+
+Compress.prototype.apply = function(compiler) {
+  const options = this.options;
+
+  const worker = workerFarm({
+    maxConcurrentWorkers,
+    maxRetries: 0,
+  }, require.resolve('./worker.js'));
+
+  compiler.plugin('compilation', compilation => {
+
+    compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+      const startTime = Date.now();
+      let files = [];
+      chunks.forEach(chunk => {
+        chunk.files.forEach(file => {
+          files.push(file);
+        });
+      });
+      compilation.additionalChunkAssets.forEach(file => {
+        files.push(file);
+      });
+      files = files.filter(f => /\js($|\?)/.test(f));
+
+      if (!files.length) return callback();
+
+      const tasks = files.map(file => {
+        const asset = compilation.assets[file];
+        const input = asset.source();
+        return new Promise((resolve, reject) => {
+          worker(input, file, options, (err, stream) => {
+            if (err) return reject(err);
+            compilation.assets[file] = new RawSource(stream);
+            resolve();
+          });
+        });
+      });
+
+      Promise.all(tasks)
+        .then(() => {
+          workerFarm.end(worker);
+          console.log(`Compress done. Cpu length: ${maxConcurrentWorkers}. Cost ${Date.now() - startTime} ms.`);
+          callback();
+        })
+        .catch(err => {
+          workerFarm.end(worker);
+          console.log(`Compress error`);
+          console.error(err);
+          callback();
+        });
+    });
+
+    compilation.plugin("normal-module-loader", context => {
+      context.minimize = true;
+    });
+  });
+};
+
+module.exports = Compress;
